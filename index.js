2287aa83dac85ad26804c7c58aa8ceab2ea0cd10
runtimeerror55 <runtimeerror55@gmail.com>
2024-01-18 17:22:59 +0530

    some changes

--- index.js
+++ index.js
@@ -55,8 +55,10 @@ passport.deserializeUser(UserModel.deserializeUser());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set("views", path.join(__dirname, "views"));
+
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 // app.get("/", (request, response) => {
 //       response.render("partials/navBar");
